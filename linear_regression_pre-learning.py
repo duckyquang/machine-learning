aa3a45d1a02f0b19e4fb063731423e8bfa2ca1bc
Duck Quang <157789469+duckyquang@users.noreply.github.com>
2025-07-29 00:35:55 +0700

    linear regression pre-learning model 1

--- linear_regression_pre-learning.py
+++ linear_regression_pre-learning.py
@@ -34,19 +34,49 @@ def meanError(weight, bias):
     sum = 0
     
     for i in range(len(y_np)):
-        sum += result(weight, bias) - y_np[i]
+        sum += result(weight, x[i], bias) - y_np[i]
     
     return sum / len(y_np)
+
+# Function to find value with lowest and highest difference
+def printMinandMax(weight, bias):
+    min = result(weight, x[0], bias) - y[0]
+    max = result(weight, x[0], bias) - y[0]
+    index_min = 0
+    index_max = 0
+    
+    for i in range(1, len(y_np)):
+        if result(weight, x[i], bias) - y[i] > max:
+            max = result(weight, x[i], bias) - y[i]
+            index_max = i
+        
+        if result(weight, x[i], bias) - y[i] < min:
+            min = result(weight, x[i], bias) - y[i]
+            index_min = i
+    
+    print("Minimum difference is: " + str(min) + ", at node "+ str(index_min))
+    print("Maximum difference is: " + str(max) + ", at node "+ str(index_max))
+
+# Function for printing algorithm output
+def returnOutput(weight, bias):
+    output = []
+    
+    for i in range(len(y_np)):
+        output.append(result(weight, x[i], bias))
+    
+    return output
     
 
 # Linear Regression algorithm
-# clean_tries = 5
-
 b = y[0]
 w = (y[len(y)-1] - y[0]) / (x[len(x)-1] - x[0])
 
-print(meanError(w, b))
-
-# while clean_tries > 0:
-# for i in x:
-    
+# Output
+print("")
+print("Orginial: " + str(y))
+print("")
+print("Predicted: " + str(returnOutput(w, b)))
+print("")
+printMinandMax(w, b)
+print("")
+print("Mean Error: " + str(meanError(w, b)))
